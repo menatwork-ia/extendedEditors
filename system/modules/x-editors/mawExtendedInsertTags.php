f998c7576e871ad9f426b1204863653dc3efd35d
Andreas Isaak <andy.jared@googlemail.com>
2013-11-13 15:25:12 +0100

    Prepare for Contao 3

--- system/modules/x-editors/mawExtendedInsertTags.php
+++ system/modules/x-editors/mawExtendedInsertTags.php
@@ -1,28 +1,9 @@
 <?php
-if (!defined('TL_ROOT')) die('You can not access this file directly!');
 
 /**
  * Contao Open Source CMS
- * Copyright (C) 2005-2010 Leo Feyer
  *
- * Formerly known as TYPOlight Open Source CMS.
- *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
- *
- * PHP version 5
- * @copyright  MEN AT WORK 2012
+ * @copyright  MEN AT WORK 2013 
  * @package    extendedEditors
  * @license    GNU/LGPL 
  * @filesource
@@ -31,7 +12,7 @@ if (!defined('TL_ROOT')) die('You can not access this file directly!');
 /**
  * Class mawExtendedInsertTags
  *
- * @copyright  MEN AT WORK 2012
+ * @copyright  MEN AT WORK 2013
  * @package    Frontend
  */
 class mawExtendedInsertTags extends Controller
@@ -102,5 +83,3 @@ class mawExtendedInsertTags extends Controller
     }
 
 }
-
-?>
