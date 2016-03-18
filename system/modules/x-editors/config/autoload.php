a5f25161b2832f6bbbf53bb23c93bb4bd716c2fd
Stefan Heimes <stefan_heimes@hotmail.com>
2016-03-18 13:46:59 +0100

    Add autoload for contao

---
+++ system/modules/x-editors/config/autoload.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * @copyright  MEN AT WORK 2014
+ * @package    extendedEditors
+ * @license    GNU/LGPL
+ * @filesource
+ */
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+    'mawExtendedInsertTags' => 'system/modules/x-editors/mawExtendedInsertTags.php'
+));
